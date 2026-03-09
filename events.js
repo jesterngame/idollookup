8971d116d52074ff6ceca9be4ea4a743d5408b63
jesterngame <xchangezero@gmail.com>
2026-03-09 20:09:27 +1000

    Create events.js

---
+++ events.js
@@ -0,0 +1,20 @@
+export const events = [
+    {
+        id:1,
+        name: "Unknown Event",
+        city: "Brisbane/Meanjin",
+        address: "Not yet known",
+        ticketlink: "",
+        groups: [2],
+        date: new Date(2026, 3, 13) // year, month (0-11), day
+    },
+    {
+        id:2,
+        name: "animaga Sunday",
+        city: "Sydney/Gadigal",
+        address: "ICC Sydney",
+        ticketlink: "http://instagram.com/animagaexpo",
+        groups: [3],
+        date: new Date(2026, 2, 15) // year, month (0-11), day
+    }
+];
