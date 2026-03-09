618a18c6e5ec17280b142a482f229dceaf6b0f4a
jesterngame <xchangezero@gmail.com>
2026-03-09 20:08:26 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import {idols} from './idols.js';
+import {idolgroups} from './idolgroups.js';
 
 console.log("test code runs");
 console.log(idols);
@@ -35,84 +36,6 @@ function makeSocials(type, link, size){
 
     return socialHtml
 }
-
-const idolgroups = [
-  {
-    id:1,
-    name:"Bloom Idol Project",
-    location:"Brisbane/Meanjin",
-    logo: "bloomlogo_400x400.jpg",
-    genre: "Jpop", 
-    description:`
-        <p>Hello Blossoms, We are Bloom Idol Project! 
-
-        <br\><br\>We are a Brisbane-based idol trio with our hearts set on captivating audiences and bringing energy and lots of sparkle to every stage we perform on! Inspired by J-pop and idol culture, our mission is to bring the crowd to life and leave you smiling! 
-
-        <br\><br\>Like flowers in a garden, we grow a little more with each performance! Whether it's your first time seeing idols or you're a long-time fan, we're here to brighten your day and bring a little joy to your heart! Join us as we bloom into your favorite idols, one petal at a time!🌸</p>
-
-        <br><i>From Youtube 9/3/26</i>
-    `,
-    socials: [
-        {
-            type: "instagram",
-            link: "https://www.instagram.com/bloom_idol_project"
-        }
-    ]
-  },
-  {
-    id:2,
-    name:"SnackTime Idols!",
-    location:"Brisbane/Meanjin",
-    logo: "snacktimelogo.jpg",
-    genre: "Jpop", 
-    description:`<p>Yum yum! We are Snack Time Idols (スナックタイム) a Kaigai Jpop Idol Trio from Brisbane. We perform singing and dancing with songs from anime, to jpop, to Australian based idol groups. Come along to watch a delicious performance that’s high energy and full of heart from our 3 appetising idols.
-
-        <br\><br\>Our goal is to grow our individual skills while performing energizing shows, perform across Australia and promote improvement, support and fun in our community</p>
-
-        <br><i>From Website 9/3/26</i>`,
-    socials: [
-        {
-            type: "instagram",
-            link: "https://www.instagram.com/snacktimeidols"
-        }
-    ]
-  },
-  {
-    id:3,
-    name:"Sunnysaurus",
-    location:"Sydney/Gadigal",
-    logo: "sunnylogo.png",
-    genre: "Jpop, Eurobeat, EDM, Rock",
-    description:`<p>
-            Passionate about community and inclusivity Sunny has been actively performing and creating live events within the Australian J-idol scene since 2019. Previously a member of MEWS Au they are now the founder and leader of idol group FAEBLE as well as a solo performer. 
-            <br\><br\>
-            Currently exploring a Mahou Gyaru concept, Sunny performs a variety of original music as well as covers that focus on happiness, confidence, fashion and magic! Their sound most typically covers Eurobeat, EDM and Rock in a j-idol format. Sunny is self produced and enjoys creating music and a variety of content across their various social media channels
-        </p>
-        <br><i>From Website 9/3/26</i>`,
-    socials: [
-        {
-            type: "instagram",
-            link: "http://instagram.com/sunnyysaurus"
-        },
-        {
-            type: "tiktok",
-            link: "https://www.tiktok.com/@sunnysaurus"
-        },
-        {
-            type: "twitter",
-            link: "https://x.com/sunnyysaurus"
-        },
-        {
-            type: "spotify",
-            link: "https://open.spotify.com/artist/0qTVRf71muCDW3EdtFcerY?si=OJaB-ISSQ5Wi4ncDano4rg"
-        },
-        {
-            type: "website",
-            link: "https://www.sunnysaurus.net/"
-        }
-    ]
-  }
-];
 
 const events = [
     {
@@ -287,6 +210,7 @@ renderIdolGroupList(idolgroups);
 $('#group-search-button').on("click", function(){
   renderIdolGroupList(idolgroups);
 });
+
 
 
 
