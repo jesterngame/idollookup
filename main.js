72e16eb008f8068862a99d26d9315f90217c3290
jesterngame <xchangezero@gmail.com>
2026-03-09 16:48:07 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import { idols } from "./idols.js"
-console.log(idols);
 function formatDate(date){
   const d = new Date(date)
   const day = String(d.getDate()).padStart(2,'0')
@@ -32,6 +30,141 @@ function makeSocials(type, link, size){
 
     return socialHtml
 }
+
+const idols = [
+    {
+        id: 1,
+        group: [1],
+        name: "Isabella",
+        image: "temp_portrait.png",
+        idoltype: "",
+        colour: "Pink",
+        socials: [
+            {
+                type: "instagram",
+                link: ""
+            },
+            {
+                type: "twitter",
+                link: ""
+            }
+        ]
+    },
+    {
+        id: 2,
+        group: [1],
+        name: "Mei",
+        image: "temp_portrait.png",
+        idoltype: "",
+        colour: "Green",
+        socials: [
+            {
+                type: "instagram",
+                link: ""
+            },
+            {
+                type: "twitter",
+                link: ""
+            }
+        ]
+    },
+    {
+        id: 3,
+        group: [1],
+        name: "Misty",
+        image: "temp_portrait.png",
+        idoltype: "",
+        colour: "Blue",
+        socials: [
+            {
+                type: "instagram",
+                link: ""
+            },
+            {
+                type: "twitter",
+                link: ""
+            }
+        ]
+    },
+    {
+        id: 4,
+        group: [2],
+        name: "Xeek",
+        image: "temp_portrait.png",
+        idoltype: "Custard Pudding",
+        colour: "Yellow",
+        socials: [
+            {
+                type: "instagram",
+                link: "https://www.instagram.com/snacktimeidols"
+            }
+        ]
+    },
+    {
+        id: 5,
+        group: [2],
+        name: "Ada",
+        image: "temp_portrait.png",
+        idoltype: "Fruit",
+        colour: "Red",
+        socials: [
+            {
+                type: "instagram",
+                link: ""
+            }
+        ]
+    },
+    {
+        id: 6,
+        group: [2],
+        name: "Chiise",
+        image: "temp_portrait.png",
+        idoltype: "Cheese",
+        colour: "Blue",
+        socials: [
+            {
+                type: "instagram",
+                link: ""
+            }
+        ]
+    },
+    {
+        id: 7,
+        group: [2],
+        name: "Hina",
+        image: "temp_portrait.png",
+        idoltype: "Soda",
+        colour: "Orange",
+        socials: [
+            {
+                type: "instagram",
+                link: ""
+            }
+        ]
+    },
+    {
+        id: 8,
+        group: [3],
+        name: "Sunny",
+        image: "imgs/sunny_pfp.jpg",
+        idoltype: "Mahou Gyaru",
+        colour: "Pink",
+        socials: [
+            {
+                type: "instagram",
+                link: "http://instagram.com/sunnyysaurus"
+            },
+            {
+                type: "tiktok",
+                link: "https://www.tiktok.com/@sunnysaurus"
+            },
+            {
+                type: "twitter",
+                link: "https://x.com/sunnyysaurus"
+            }
+        ]
+    },
+];
 
 const idolgroups = [
   {
@@ -260,5 +393,6 @@ function renderIdolGroup(id){
 
 
 renderIdolGroupList();
+
 
 
