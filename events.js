5da4f5869aee09ae33530f6adbffbdbb3c1d3f7e
jesterngame <xchangezero@gmail.com>
2026-03-10 11:41:13 +1000

    Update events.js

--- events.js
+++ events.js
@@ -4,6 +4,7 @@ export const events = [
         name: "Unknown Event",
         city: "Brisbane/Meanjin",
         address: "Not yet known",
+        description: `<p>An event being put on by Snacktime</p>`,
         ticketlink: "",
         groups: [2],
         date: new Date(2026, 3, 13) // year, month (0-11), day
@@ -13,6 +14,7 @@ export const events = [
         name: "animaga Sunday",
         city: "Sydney/Gadigal",
         address: "ICC Sydney",
+        description: `<p>Mark your calendars! Animaga is coming to ICC Sydney on 14–15 March 2026, bringing two unforgettable days packed with anime, fandom, and excitement. For the very first time, Animaga is setting the stage in Sydney, and we're ready to make it an event to remember.</p>`,
         ticketlink: "http://instagram.com/animagaexpo",
         groups: [3],
         date: new Date(2026, 2, 15) // year, month (0-11), day
@@ -22,7 +24,28 @@ export const events = [
         name: "Full Throttle Idol Relay Live",
         city: "Brisbane/Meanjin",
         address: "Hamilton Town Hall",
+        description: `<p>VIP DOORS OPEN: 5:00PM
+                <br/><br/>
+                GA DOORS OPEN: 5:30PM
+                <br/><br/>
+                Join us for the ultimate Idol Taiban Event where sweat flies, wotas roar, and only the toughest idols make it through. This is the Full Throttle Idol Relay Live. Three idol racers go in, one comes out the winner!
+                <br/><br/>
+                Three Idols will take the stage to show everyone what they are made of! This live is a race to the end of the setlist concept. Each group will have five songs to complete before the end of the show. The first group to complete all five songs wins! The winning idols will have bragging rights and a solo encore song as a victory lap!
+                <br/><br/>
+                Want to show your support for your favourite idol? VIPs get the opportunity to influence the race by picking their favourite idol. The winning team gets a special surprise for supporting their oshi to the end!
+                <br/><br/>
+                Will your favourite idol win?
+                <br/><br/>
+                SHOW FEATURES THE FOLLOWING IDOLS
+                <br/><br/>
+                    CASSIE SAMA
+                <br/><br/>
+                    CH4I
+                <br/><br/>
+                    AMUSE PROJECT (Rinya, Pengi, Luvkii and Aleks)
+                <p/>`,
         ticketlink: "https://events.humanitix.com/full-throttle-idol-relay-live",
+        img: "imgs/full_throttle_idol_relay_live.jpg",
         groups: [5,6,7],
         date: new Date(2026, 2, 14) // year, month (0-11), day
     }
