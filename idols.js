f0ac524c9919306c1f41392da2ff62cf5125986b
jesterngame <xchangezero@gmail.com>
2026-03-13 17:04:59 +1000

    Update idols.js

--- idols.js
+++ idols.js
@@ -3,7 +3,6 @@ export const idols = [
         id: 1,
         group: [1],
         name: "Isabella",
-        image: "imgs/temp_pfp.png",
         idoltype: "",
         colour: "Pink",
         socials: [
@@ -21,7 +20,6 @@ export const idols = [
         id: 2,
         group: [1],
         name: "Mei",
-        image: "imgs/temp_pfp.png",
         idoltype: "",
         colour: "Green",
         socials: [
@@ -39,7 +37,6 @@ export const idols = [
         id: 3,
         group: [1],
         name: "Misty",
-        image: "imgs/temp_pfp.png",
         idoltype: "",
         colour: "Blue",
         socials: [
@@ -57,7 +54,6 @@ export const idols = [
         id: 4,
         group: [2],
         name: "Xeek",
-        image: "imgs/temp_pfp.png",
         idoltype: "Custard Pudding",
         colour: "Yellow",
         socials: [
@@ -71,7 +67,6 @@ export const idols = [
         id: 5,
         group: [2],
         name: "Ada (hiatus)",
-        image: "imgs/temp_pfp.png",
         idoltype: "Fruit",
         colour: "Red",
         socials: [
@@ -85,7 +80,6 @@ export const idols = [
         id: 6,
         group: [2],
         name: "Chiise",
-        image: "imgs/temp_pfp.png",
         idoltype: "Cheese",
         colour: "Blue",
         socials: [
@@ -97,18 +91,8 @@ export const idols = [
     },
     {
         id: 7,
-        group: [2],
-        name: "Hina",
-        image: "imgs/temp_pfp.png",
-        idoltype: "Soda",
-        colour: "Orange",
-        socials: []
-    },
-    {
-        id: 8,
         group: [3],
         name: "Sunny",
-        image: "imgs/sunny_pfp.jpg",
         idoltype: "Mahou Gyaru",
         colour: "Pink",
         socials: [
@@ -127,106 +111,97 @@ export const idols = [
         ]
     },
     {
-        id: 9,
+        id: 8,
         group: [4],
         name: "May",
-        image: "imgs/temp_pfp.png",
         idoltype: "",
         colour: "Purple",
         socials: [
         ]
     },
     {
-        id: 10,
+        id: 9,
         group: [4],
         name: "Ruru",
-        image: "imgs/temp_pfp.png",
         idoltype: "",
         colour: "Pink",
+        socials: [
+        ]
+    },
+    {
+        id: 10,
+        group: [5],
+        name: "Lolly (hiatus)",
+        idoltype: "",
+        colour: "Purple",
         socials: [
         ]
     },
     {
         id: 11,
         group: [5],
-        name: "Lolly (hiatus)",
-        image: "imgs/temp_pfp.png",
-        idoltype: "",
-        colour: "Purple",
-        socials: [
-        ]
-    },
-    {
-        id: 12,
-        group: [5],
         name: "Kate",
-        image: "imgs/temp_pfp.png",
         idoltype: "",
         colour: "Blue",
         socials: [
         ]
     },
     {
-        id: 13,
+        id: 12,
         group: [5],
         name: "Rinya",
-        image: "imgs/temp_pfp.png",
         idoltype: "",
         colour: "Monochrome",
         socials: [
         ]
     },
     {
-        id: 14,
+        id: 13,
         group: [5],
         name: "Pengi",
-        image: "imgs/temp_pfp.png",
         idoltype: "",
         colour: "Pink",
         socials: [
         ]
     },
     {
-        id: 15,
+        id: 14,
         group: [5],
         name: "Luvkii",
-        image: "imgs/temp_pfp.png",
         idoltype: "",
         colour: "Red",
         socials: [
         ]
     },
     {
-        id: 16,
+        id: 15,
         group: [5],
         name: "ALEKS",
-        image: "imgs/temp_pfp.png",
         idoltype: "",
         colour: "Yellow",
         socials: [
         ]
     },
     {
-        id: 17,
+        id: 16,
         group: [6],
         name: "Cassie-Sama",
-        image: "imgs/temp_pfp.png",
         idoltype: "",
         colour: "Purple",
         socials: [
         ]
     },
     {
-        id: 18,
+        id: 17,
         group: [7],
         name: "Ch4i",
-        image: "imgs/temp_pfp.png",
         idoltype: "",
         colour: "Green",
         socials: [
         ]
     }
 ];
+
 
 
 
