488d9e87077bc07e55cc067f93ab7e1bb64f26e4
jesterngame <xchangezero@gmail.com>
2026-03-13 21:33:36 +1000

    Update idols.js

--- idols.js
+++ idols.js
@@ -3,6 +3,7 @@ export const idols = [
         id: 1,
         group: [1],
         name: "Isabella",
+        image: "imgs/temp_portrait.png",
         idoltype: "",
         colour: "Pink",
         socials: [
@@ -20,6 +21,7 @@ export const idols = [
         id: 2,
         group: [1],
         name: "Mei",
+        image: "imgs/temp_portrait.png",
         idoltype: "",
         colour: "Green",
         socials: [
@@ -37,6 +39,7 @@ export const idols = [
         id: 3,
         group: [1],
         name: "Misty",
+        image: "imgs/temp_portrait.png",
         idoltype: "",
         colour: "Blue",
         socials: [
@@ -54,6 +57,7 @@ export const idols = [
         id: 4,
         group: [2],
         name: "Xeek",
+        image: "imgs/temp_portrait.png",
         idoltype: "Custard Pudding",
         colour: "Yellow",
         socials: [
@@ -67,6 +71,7 @@ export const idols = [
         id: 5,
         group: [2],
         name: "Ada (hiatus)",
+        image: "imgs/temp_portrait.png",
         idoltype: "Fruit",
         colour: "Red",
         socials: [
@@ -80,6 +85,7 @@ export const idols = [
         id: 6,
         group: [2],
         name: "Chiise",
+        image: "imgs/temp_portrait.png",
         idoltype: "Cheese",
         colour: "Blue",
         socials: [
@@ -90,9 +96,10 @@ export const idols = [
         ]
     },
     {
-        id: 7,
-        group: [3],
+        id: 8,
+        group: [3, 15],
         name: "Sunny",
+        image: "",
         idoltype: "Mahou Gyaru",
         colour: "Pink",
         socials: [
@@ -111,98 +118,456 @@ export const idols = [
         ]
     },
     {
-        id: 8,
-        group: [4],
-        name: "May",
-        idoltype: "",
-        colour: "Purple",
-        socials: [
-        ]
-    },
-    {
         id: 9,
         group: [4],
-        name: "Ruru",
+        name: "May",
+        image: "imgs/temp_portrait.png",
         idoltype: "",
-        colour: "Pink",
+        colour: "Purple",
         socials: [
         ]
     },
     {
         id: 10,
-        group: [5],
-        name: "Lolly (hiatus)",
+        group: [4],
+        name: "Ruru",
+        image: "imgs/temp_portrait.png",
         idoltype: "",
-        colour: "Purple",
+        colour: "Pink",
         socials: [
         ]
     },
     {
         id: 11,
         group: [5],
-        name: "Kate",
-        idoltype: "",
-        colour: "Blue",
-        socials: [
-        ]
-    },
-    {
-        id: 12,
-        group: [5],
-        name: "Rinya",
-        idoltype: "",
-        colour: "Monochrome",
-        socials: [
-        ]
-    },
-    {
-        id: 13,
-        group: [5],
-        name: "Pengi",
-        idoltype: "",
-        colour: "Pink",
-        socials: [
-        ]
-    },
-    {
-        id: 14,
-        group: [5],
-        name: "Luvkii",
-        idoltype: "",
-        colour: "Red",
-        socials: [
-        ]
-    },
-    {
-        id: 15,
-        group: [5],
-        name: "ALEKS",
-        idoltype: "",
-        colour: "Yellow",
-        socials: [
-        ]
-    },
-    {
-        id: 16,
-        group: [6],
-        name: "Cassie-Sama",
+        name: "Lolly (hiatus)",
+        image: "imgs/temp_portrait.png",
         idoltype: "",
         colour: "Purple",
         socials: [
         ]
     },
     {
+        id: 12,
+        group: [5],
+        name: "Kate",
+        image: "imgs/temp_portrait.png",
+        idoltype: "",
+        colour: "Blue",
+        socials: [
+        ]
+    },
+    {
+        id: 13,
+        group: [5],
+        name: "Rinya",
+        image: "imgs/temp_portrait.png",
+        idoltype: "",
+        colour: "Monochrome",
+        socials: [
+        ]
+    },
+    {
+        id: 14,
+        group: [5],
+        name: "Pengi",
+        image: "imgs/temp_portrait.png",
+        idoltype: "",
+        colour: "Pink",
+        socials: [
+        ]
+    },
+    {
+        id: 15,
+        group: [5],
+        name: "Luvkii",
+        image: "imgs/temp_portrait.png",
+        idoltype: "",
+        colour: "Red",
+        socials: [
+        ]
+    },
+    {
+        id: 16,
+        group: [5],
+        name: "ALEKS",
+        image: "imgs/temp_portrait.png",
+        idoltype: "",
+        colour: "Yellow",
+        socials: [
+        ]
+    },
+    {
         id: 17,
+        group: [6],
+        name: "Cassie-Sama",
+        image: "imgs/temp_portrait.png",
+        idoltype: "",
+        colour: "Purple",
+        socials: [
+        ]
+    },
+    {
+        id: 18,
         group: [7],
         name: "Ch4i",
+        image: "imgs/temp_portrait.png",
         idoltype: "",
         colour: "Green",
         socials: [
         ]
+    },
+        {
+        id: 19,
+        group:[8],
+        name: "Scarlet",
+        image: "",
+        idoltype: "New Love",
+        colour: "Rose Pink",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/scarlet_melody.chu/"
+            }
+        ]
+    },
+    {
+        id: 20,
+        group:[8],
+        name: "Ari",
+        image: "",
+        idoltype: "Obsessive Love ",
+        colour: "Angelic White",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/_as2u.u/"
+            }
+        ]
+    },
+    {
+        id: 21,
+        group:[8],
+        name: "Pita",
+        image: "",
+        idoltype: "Lost Love",
+        colour: "Stormy Blue",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/pita_rye/"
+            }
+        ]
+    },
+    {
+        id: 22,
+        group:[8],
+        name: "Julie",
+        image: "",
+        idoltype: "Self Love",
+        colour: "Electric Yellow",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/z.julie.z/"
+            }
+        ]
+    },
+    {
+        id: 23,
+        group:[9],
+        name: "Lace",
+        image: "",
+        idoltype: "Witch Class President",
+        colour: "Deep Lilac",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/little.lace.doll/"
+            }
+        ]
+    },
+    {
+        id: 24,
+        group:[9],
+        name: "Kim",
+        image: "",
+        idoltype: "Siren Band Captain",
+        colour: "Columbia Blue",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/daisukiim"
+            }
+        ]
+    },
+    {
+        id: 25,
+        group:[9],
+        name: "Venus",
+        image: "",
+        idoltype: "Ghostly School Nurse",
+        colour: "Ghost Grey",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/venusbunz/"
+            }
+        ]
+    },
+    {
+        id: 26,
+        group:[10],
+        name: "Darc",
+        image: "",
+        idoltype: "Vampire",
+        colour: "Crimson Red",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/dracubexa/"
+            }
+        ]
+    },
+    {
+        id: 27,
+        group:[10],
+        name: "Fungi",
+        image: "",
+        idoltype: "Zombie",
+        colour: "Lavender",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/kowai.fungi/"
+            }
+        ]
+    },
+    {
+        id: 28,
+        group:[11],
+        name: "Bibi",
+        image: "",
+        idoltype: "Shark",
+        colour: "Hot Pink",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/bibimooncosplays/"
+            }
+        ]
+    },
+    {
+        id: 29,
+        group:[11],
+        name: "JINX",
+        image: "",
+        idoltype: "Demon Llama",
+        colour: "Neon Green",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/jinx.boo.cos/"
+            }
+        ]
+    },
+    {
+        id: 30,
+        group:[12],
+        name: "Sakura",
+        image: "",
+        idoltype: "",
+        colour: "Pink",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/s2ulka/"
+            }
+        ]
+    },
+    {
+        id: 31,
+        group:[12],
+        name: "Yue",
+        image: "",
+        idoltype: "",
+        colour: "Yellow",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/yuezyume/"
+            }
+        ]
+    },
+    {
+        id: 32,
+        group:[12],
+        name: "Eimi",
+        image: "",
+        idoltype: "",
+        colour: "Green",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/eimiterasu/"
+            }
+        ]
+    },
+    {
+        id: 33,
+        group:[12],
+        name: "Maryui",
+        image: "",
+        idoltype: "",
+        colour: "Red",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/m.maruiiii0703__/"
+            }
+        ]
+    },
+    {
+        id: 34,
+        group:[13],
+        name: "Naneko",
+        image: "",
+        idoltype: "",
+        colour: "Blue",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/naneko_nya/"
+            }
+        ]
+    },
+    {
+        id: 35,
+        group:[14],
+        name: "FUUTAiZMiZUKi",
+        image: "",
+        idoltype: "",
+        colour: "Royal Blue",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/fuutatoaus0201/"
+            }
+        ]
+    },
+    {
+        id: 36,
+        group:[14],
+        name: "Shiitakki ",
+        image: "",
+        idoltype: "",
+        colour: "Royal Purple",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/shii.takki/"
+            }
+        ]
+    },
+    {
+        id: 37,
+        group:[15],
+        name: "Coda",
+        image: "",
+        idoltype: "",
+        colour: "Nostalgic Blue",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://instagram.com/undeadcoda"
+            }
+        ]
+    },
+    {
+        id: 38,
+        group:[16],
+        name: "Emi",
+        image: "",
+        idoltype: "",
+        colour: "Purple",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/its.emi0_0/"
+            }
+        ]
+    },
+    {
+        id: 39,
+        group:[16],
+        name: "Lili",
+        image: "",
+        idoltype: "",
+        colour: "Blue",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/lililovechu/"
+            }
+        ]
+    },
+    {
+        id: 40,
+        group:[17],
+        name: "DAV!NE",
+        image: "",
+        idoltype: "",
+        colour: "Light Blue",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/davinegravity/"
+            }
+        ]
+    },
+    {
+        id: 41,
+        group:[18],
+        name: "Juju",
+        image: "",
+        idoltype: "Cutesy Pink Succubus",
+        colour: "Devilish Pink",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/jujueel"
+            }
+        ]
+    },
+    {
+        id: 42,
+        group:[18],
+        name: "Kim",
+        image: "",
+        idoltype: "Seal-y Blue Siren",
+        colour: "Ocean Wave Blue",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/daisukiim"
+            }
+        ]
+    },
+    {
+        id: 43,
+        group:[19],
+        name: "Tay",
+        image: "",
+        idoltype: "Demon prince",
+        colour: "Red",
+        socials: [
+            {
+                 type: "instagram",
+                link: "https://www.instagram.com/tayytrent/"
+            }
+        ]
     }
 ];
-
-
 
 
 
